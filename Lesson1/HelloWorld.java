c620e929e65d47e6dd95236e320a4891bc1367eb
KonstantinLeineveber <elbpat@gmail.com>
2021-01-12 23:32:09 +0300

    Hello world example

---
+++ Lesson1/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+	public static void main(String ... args) throws Exception {
+		
+		while(true) {
+			System.out.println("Hello Kostya!!!!");
+			Thread.sleep(1000);
+		}
+
+	}
+
+}
