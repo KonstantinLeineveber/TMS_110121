08aed71ebcf4a51312aec128e82dc0286b66d96a
KonstantinLeineveber <elbpat@gmail.com>
2021-02-01 18:23:41 +0300

    исправлена 14 задача

--- lesson5/src/other/hw1.java
+++ lesson5/src/other/hw1.java
@@ -5,12 +5,13 @@ public class hw1 {
 //        Начав тренировки, спортсмен в первый день пробежал 10 км.
 //        Каждый день он увеличивал дневную норму на 10% нормы предыдущего дня.
 //        Какой суммарный путь пробежит спортсмен за 7 дней?
-        int preSumm = 10;
-        int summ = 10;
+        double preSumm = 10;
+        double summ = 10;
         for (int beg = 1; beg < 7; beg++) {
             preSumm *= 1.1;
             summ += preSumm;
         }
+        summ =  (int) summ;
         System.out.println(summ);
     }
 }
